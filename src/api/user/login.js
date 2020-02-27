8dc8b0937d9f0fb91dcee3129b3e9948ae0fae05
ajay22p <ajay.p@tibilsolutions.com>
2020-02-27 13:15:41 +0530

    firstversion

---
+++ src/api/user/login.js
@@ -0,0 +1,25 @@
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../../lib/config');
+const user = require('../../data/user');
+const express = require("express");
+const router = express.Router();
+router.post('/login', (req, res) => {
+    user.findOne({ email: req.body.email }, (err, user) => {
+      if (err) return res.status(500).send('Error on the server.');
+      if (!user) return res.status(404).send('No user found.');
+   
+      var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+      if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+  
+      //token code
+      var token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 86400 // expires in 24 hours
+      });
+  
+      res.status(200).send({ auth: true, token: token });
+    });
+  
+
+  });
+  module.exports = router;
---
+++ src/lib/config.js
@@ -0,0 +1,3 @@
+module.exports= {
+    'secret' : 'hello_tibil'
+}
